43d34e4d3dbc50aa9c0fed983da70a668a7d028b
Vanessa Martinez <vmar76@gmail.com>
2020-02-11 20:14:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,15 +7,15 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 / num2
 end
 
 def square_root(num)
